a52efdd5da1ba2141d0ac2e6b9f94e77a93a2b6d
apronoob88 <apronoob88@gmail.com>
2019-11-22 14:25:37 +0800

    Test Draft
    - Added comments

--- 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Blk57.java
+++ 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Blk57.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Blk57 extends AppCompatActivity {
-
+    // Test commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
