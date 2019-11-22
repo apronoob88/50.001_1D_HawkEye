fd20d7bf771523c11a9fed0f0b36a1072b3edf9a
apronoob88 <apronoob88@gmail.com>
2019-11-22 15:07:58 +0800

    Update Blk57.java
    added haha

--- 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Blk57.java
+++ 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Blk57.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Blk57 extends AppCompatActivity {
     // Test commit
+    //haha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
