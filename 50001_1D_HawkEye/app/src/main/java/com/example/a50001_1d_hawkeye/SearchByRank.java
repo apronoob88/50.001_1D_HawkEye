69e8bbbe8c0f0f1e0265ae46c15888f5018f7ad6
wilbertaristo <wilbert.aristo@hotmail.com>
2019-12-01 21:06:08 +0800

    Revert "Revert "unbroken""
    This reverts commit 08d4d6335a12b20ac43ebefe3ef4bdb5c95911dd.

--- 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/SearchByRank.java
+++ 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/SearchByRank.java
@@ -2,7 +2,9 @@ package com.example.a50001_1d_hawkeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SearchByRank extends AppCompatActivity {
