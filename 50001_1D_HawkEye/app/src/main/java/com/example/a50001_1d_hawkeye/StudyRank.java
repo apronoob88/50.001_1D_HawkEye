79e8b5efaf92b8440969b357db9e1d858a22037e
apronoob88 <apronoob88@gmail.com>
2019-12-06 01:21:07 +0800

    Revert "Revert "occupancy rate all working""
    This reverts commit b2f9a6bbd4258e50e9677c9859c7430e945866cb.

--- 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/StudyRank.java
+++ 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/StudyRank.java
@@ -3,6 +3,7 @@ package com.example.a50001_1d_hawkeye;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -59,6 +60,17 @@ public class StudyRank extends AppCompatActivity {
                 else{
                     ivOccupationRate.setImageResource(R.drawable.red);
                 }
+                final String locationClicked = getRef(position).getKey();
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent moreDetailed = new Intent(StudyRank.this, SearchByRankDetails.class);
+                        moreDetailed.putExtra("Name",locationClicked);
+                        moreDetailed.putExtra("category","Study");
+                        startActivity(moreDetailed);//Start the new activity
+
+                    }
+                });
             }
         };
         listView.setAdapter(adapter);
