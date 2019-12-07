9f3b209e79e098d039651e8d729a9fe5094121ad
jeroee <jeroejeroe@gmail.com>
2019-12-07 12:39:43 +0800

    final

--- 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Location.java
+++ 50001_1D_HawkEye/app/src/main/java/com/example/a50001_1d_hawkeye/Location.java
@@ -14,8 +14,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DecimalFormat;
+
 public class Location extends AppCompatActivity {
-    private TextView name,number, totalCapacity, occupancyRate;
+    private TextView name,number, totalCapacity, occupancyRate, nameContent, numberContent, totalCapacityContent, occupancyRateContent;
     private ImageView picture;
     DatabaseReference reff;
 
@@ -27,6 +29,10 @@ public class Location extends AppCompatActivity {
         number = (TextView)findViewById(R.id.number);
         totalCapacity = (TextView)findViewById(R.id.totalCapacity);
         occupancyRate = (TextView)findViewById(R.id.occupancyRate);
+        nameContent = (TextView) findViewById(R.id.nameContent);
+        numberContent = (TextView) findViewById(R.id.numberContent);
+        totalCapacityContent = (TextView) findViewById(R.id.totalCapacityContent);
+        occupancyRateContent = (TextView) findViewById(R.id.occupancyRateContent);
 
         picture=(ImageView)findViewById(R.id.picture);
         //get intent from the previous page, picture set to canteen by default
@@ -40,14 +46,24 @@ public class Location extends AppCompatActivity {
         reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String numberInner = "number of people: "+dataSnapshot.child(checkLocation).child("number").getValue().toString();
-                String totalCapacityInner = "total capacity: "+dataSnapshot.child(checkLocation).child("total capacity").getValue().toString();
-                String occupancyRateInner = "total occupancy rate: "+ dataSnapshot.child(checkLocation).child("occupancy rate").getValue().toString()+"%";
-                String nameInner = "location: "+dataSnapshot.child(checkLocation).child("name").getValue().toString();
+                DecimalFormat df = new DecimalFormat();
+                df.setMaximumFractionDigits(2);
+                String numberInner = "Number of people: ";
+                String totalCapacityInner = "Total capacity: ";
+                String occupancyRateInner = "Total occupancy rate: ";
+                String nameInner = "Location: ";
+                String numberContent1 = dataSnapshot.child(checkLocation).child("number").getValue().toString();
+                String totalCapacityContent1 = dataSnapshot.child(checkLocation).child("total capacity").getValue().toString();
+                float occupancyRateFloat = Math.round((Float.parseFloat(numberContent1) / Float.parseFloat(totalCapacityContent1)) * 100);
+                String nameContent1 = dataSnapshot.child(checkLocation).child("name").getValue().toString();
                 name.setText(nameInner);
                 number.setText(numberInner);
                 totalCapacity.setText(totalCapacityInner);
                 occupancyRate.setText(occupancyRateInner);
+                nameContent.setText(nameContent1);
+                numberContent.setText(numberContent1);
+                totalCapacityContent.setText(totalCapacityContent1);
+                occupancyRateContent.setText(Float.toString(occupancyRateFloat) + "%");
             }
 
             @Override
