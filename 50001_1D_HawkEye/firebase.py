25261c6de9f7b312fbbccbea8ee2c0668618f557
Hsengiv2000 <46417635+Hsengiv2000@users.noreply.github.com>
2019-12-16 17:57:29 +0800

    Update firebase.py
    With comments

--- 50001_1D_HawkEye/firebase.py
+++ 50001_1D_HawkEye/firebase.py
@@ -1,8 +1,9 @@
-from libdw import pyrebase
+from libdw import pyrebase #Using the DW library for firebase
 import cv2
 import numpy as np 
 from PIL import Image
 def change_value():
+    #Firebase details
     url ="https://fir-test-8c15d.firebaseio.com/"#"https://java1d.firebaseio.com/"
     apikey = "AIzaSyCOj5VmBPcENI3WuZaXnwEdc016wk-Xe7k"#"AIzaSyD_szQHeqkFF9f4FzZzh0FBseupcV8O0kk"
     config = {"apiKey": apikey,"databaseURL": url }#required details for the firebase database
@@ -10,7 +11,7 @@ def change_value():
     db =firebase.database()#creating a database object to acccess firebase realtime database
     print("Trying to authorize " )
     try:
-        f = open("roomcount.txt", "r", encoding = "utf_8")
+        f = open("roomcount.txt", "r", encoding = "utf_8") #This text file was written by our java code that contains room name and room count
         print("herrrr")
 
         namecount = []
@@ -20,10 +21,10 @@ def change_value():
       
       
         
-        db.child("Locations").child(namecount[0]).child("number").set(int(namecount[1]))#Giving authorization to the student ID that was sent as input
-        db.child("Locations").child(namecount[0]).child("occupancy rate").set(int(50))
+        db.child("Locations").child(namecount[0]).child("number").set(int(namecount[1]))#Setting the locations room number to the number of perople obtained from the text file
+        db.child("Locations").child(namecount[0]).child("occupancy rate").set(int(50))#Similar for occupancy rate
         print(5)
-        total = db.child("Locations").child(namecount[0]).child("total capacity").get().val()
+        total = db.child("Locations").child(namecount[0]).child("total capacity").get().val()#Similar for total capacity
         db.child("Locations").child(namecount[0]).child("occupancy rate").set(int(namecount[1])/int(total))
 
         print("Authorization successful")
